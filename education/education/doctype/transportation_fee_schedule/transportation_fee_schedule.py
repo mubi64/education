6eeb5ef5609aa8fbb57ecc616f34dd7e3025f725
Muhammad Saad <72243425+saadsafda@users.noreply.github.com>
2025-04-07 15:11:31 +0500

    Update Fee schedule

--- education/education/doctype/transportation_fee_schedule/transportation_fee_schedule.py
+++ education/education/doctype/transportation_fee_schedule/transportation_fee_schedule.py
@@ -4,10 +4,9 @@
 import erpnext
 import frappe
 from frappe import _
-
 from frappe.model.document import Document
 from frappe.utils.background_jobs import enqueue
-from frappe.utils import cstr, add_months, add_days, flt
+from frappe.utils import add_months, add_days
 from education.education.api import get_current_enrollment
 
 
@@ -23,22 +22,12 @@ class TransportationFeeSchedule(Document):
 			"currency": erpnext.get_company_currency(self.company),
 		}
 
-		# fees_amount = frappe.db.sql(
-		# 	"""select sum(grand_total), sum(outstanding_amount) from tabFees
-		# 	where fee_schedule=%s and docstatus=1""",
-		# 	(self.name),
-		# )
-
-		# if fees_amount:
-		# 	info["total_paid"] = flt(fees_amount[0][0]) - \
-		# 		flt(fees_amount[0][1])
-		# 	info["total_unpaid"] = flt(fees_amount[0][1])
-
 		return info
 
 	def validate(self):
 		if self.total_student == 0:
 			frappe.throw(_("Please get student first"))
+
 	@frappe.whitelist()
 	def create_fees(self):
 		if self.total_student > 10:
@@ -62,91 +51,88 @@ class TransportationFeeSchedule(Document):
 			["transportation_fee_structure", "is", "set"],
 			["enabled", "=", 1]
 		], fields=['name'])
-		if len(students) > 0:
-			created_records = 0
-			error = False
-			for st in students:
+
+		if not students:
+			frappe.throw("There are no students with a transportation fee structure enabled.")
+
+		created_records = 0
+		error = False
+		error_logs = []
+
+		for st in students:
+			try:
 				enrollment = get_current_enrollment(st.name)
-				# print(student, self.posting_date, self.due_date, "cchek student")
+				if not enrollment:
+					error = True
+					error_logs.append(f"Enrollment not found for student {st.name}")
+					continue
+
 				one_month_earlier = add_months(self.posting_date, -1)
 				result_date = add_days(one_month_earlier, 1)
-				# print(enrollment, "testing **************************")
-				# frappe.throw(str(enrollment))
+
 				student = frappe.get_doc("Student", st.name, fields=['*'])
 				trans_student = frappe.get_doc("Transportation Fee Structure", student.transportation_fee_structure, fields=['*'])
-				fee = frappe.db.get_all("Fees", filters=[
-						['docstatus', "!=", 2],
-						['posting_date', 'between', [result_date, self.posting_date]],
-						['student', "=", student.name],
-						['Fee Component', 'fees_category', '=', trans_student.fee_category]
-					], fields=["*"])
-				if len(fee) == 0:
-					try:
-						# fees_category_array = []
-						stu_months_array = []
-						tran_months_array = []
+				
+				# Check if fee already exists
+				existing_fees = frappe.db.get_all("Fees", filters=[
+					['docstatus', "!=", 2],
+					['posting_date', 'between', [result_date, self.posting_date]],
+					['student', "=", student.name],
+					['Fee Component', 'fees_category', '=', trans_student.fee_category]
+				], fields=["name"])
 
-						# for comp in self.components:
-						# 	fees_category_array.append(comp.fees_category)
+				if existing_fees:
+					continue  # Skip if fee already exists
 
-						for m in student.transportation_fee_structure_months:
-							stu_months_array.append(m.month_number)
-
-						for m in trans_student.transportation_fee_structure_months:
-							tran_months_array.append(m.month_number)
-						posting_month = frappe.utils.formatdate(self.posting_date, "MM")
-						if frappe.utils.getdate(str(self.posting_date)) >= frappe.utils.getdate(str(student.start_date)):
-							if posting_month in stu_months_array or posting_month not in tran_months_array:
-								pass
-								# for comp in self.components:
-								# 	if comp.fees_category == trans_student.fee_category:
-								# 		self.components.remove(comp)
-							# el
-							elif (posting_month in tran_months_array): # and trans_student.dependant_fee_category in fees_category_array
-								fee = frappe.new_doc('Fees')
-								fee.student = student.name
-								fee.program = enrollment.program
-								fee.posting_date = self.posting_date
-								fee.transportation_fee_schedule = self.name
-								fee.due_date = self.due_date
-								fee.components = []
-								fee.append('components', {
-									"fees_category": trans_student.fee_category,
-									"amount": trans_student.fee_amount,
-									"gross_amount": trans_student.fee_amount
-								})
-								fee.taxes_and_charges = self.taxes_and_charges
-								fee.taxes = self.taxes
-								if self.receivable_account:
-									fee.receivable_account = self.receivable_account
-								if self.income_account:
-									fee.income_account = self.income_account
-								fee.save()
-								# if trans_student.fee_category not in fees_category_array:
-								#     row = self.append('components', {})
-								#     row.fees_category = trans_student.fee_category
-								#     row.amount = trans_student.fee_amount
-								#     row.gross_amount = trans_student.fee_amount
-								# else:
-								#     for comp in self.components:
-								#         if comp.fees_category == trans_student.fee_category:
-								#             comp.amount = trans_student.fee_amount
-								#             comp.gross_amount = trans_st
-								created_records += 1
-								# frappe.show_progress("Progress", created_records, self.total_student, 'Please Wait')
-					except Exception as e:
-						error = True
-						err_msg = (
-							frappe.local.message_log and "\n\n".join(
-								frappe.local.message_log) or cstr(e)
-						)
-		else:
-			frappe.throw("There are no transportation fee structure")
+				tran_months_array = [m.month_number for m in trans_student.transportation_fee_structure_months]
+				posting_month = frappe.utils.formatdate(self.posting_date, "MM")
+				if frappe.utils.getdate(str(self.posting_date)) >= frappe.utils.getdate(str(student.start_date)):
+					if (posting_month in tran_months_array): # and trans_student.dependant_fee_category in fees_category_array
+						fee = frappe.new_doc('Fees')
+						fee.student = student.name
+						fee.program = enrollment.program
+						fee.posting_date = self.posting_date
+						fee.transportation_fee_schedule = self.name
+						fee.due_date = self.due_date
+						fee.components = []
+						fee.append('components', {
+							"fees_category": trans_student.fee_category,
+							"amount": trans_student.fee_amount,
+							"gross_amount": trans_student.fee_amount
+						})
+						fee.taxes_and_charges = self.taxes_and_charges
+						for tax in self.taxes:
+							fee.append("taxes", {
+								"charge_type": tax.charge_type,
+								"description": tax.description,
+								"account_head": tax.account_head,
+								"included_in_print_rate": tax.included_in_print_rate,
+								"cost_center": tax.cost_center,
+								"rate": tax.rate,
+								"account_currency": tax.account_currency,
+								"base_tax_amount": tax.base_tax_amount,
+								"tax_amount": tax.tax_amount,
+								"base_total": tax.base_total,
+								"total": tax.total,
+								"tax_amount_after_discount_amount": tax.tax_amount_after_discount_amount 
+							})
+						# fee.taxes = 
+						if self.receivable_account:
+							fee.receivable_account = self.receivable_account
+						if self.income_account:
+							fee.income_account = self.income_account
+						fee.save()
+						created_records += 1
+			except Exception as e:
+				error = True
+				err_msg = frappe.get_traceback()
+				error_logs.append(f"Error for student {st.name}: {err_msg}")
 		if error:
 			frappe.db.rollback()
 			frappe.db.set_value("Transportation Fee Schedule", self.name,
 								"fee_creation_status", "Failed")
-			frappe.db.set_value("Transportation Fee Schedule", self.name, "error_log", err_msg)
+			frappe.db.set_value("Transportation Fee Schedule", self.name,
+							"error_log", "\n".join(error_logs))
 
 		else:
 			frappe.db.set_value("Transportation Fee Schedule", self.name,
@@ -165,60 +151,3 @@ class TransportationFeeSchedule(Document):
 		])
 
 		return len(students)
-	
-	# def append_transportation(self):
-    #     fee_student = frappe.get_doc('Student', self.student)
-    #     if fee_student.transportation_fee_structure:
-    #         trans_student = frappe.get_doc(
-    #             'Transportation Fee Structure', fee_student.transportation_fee_structure, fields=['*'])
-    #         fees_category_array = []
-    #         stu_months_array = []
-    #         tran_months_array = []
-
-    #         for comp in self.components:
-    #             fees_category_array.append(comp.fees_category)
-
-    #         for m in fee_student.transportation_fee_structure_months:
-    #             stu_months_array.append(m.month_number)
-
-    #         for m in trans_student.transportation_fee_structure_months:
-    #             tran_months_array.append(m.month_number)
-                
-    #         posting_month = frappe.utils.formatdate(self.posting_date, "MM")
-    #         if frappe.utils.getdate(str(self.posting_date)) >= frappe.utils.getdate(str(fee_student.start_date)):
-    #             if posting_month in stu_months_array or posting_month not in tran_months_array:
-    #                 for comp in self.components:
-    #                     if comp.fees_category == trans_student.fee_category:
-    #                         self.components.remove(comp)
-    #             elif (posting_month in tran_months_array 
-    #                     and trans_student.dependant_fee_category in fees_category_array):
-    #                     pass
-    #                     # if trans_student.fee_category not in fees_category_array:
-    #                     #     row = self.append('components', {})
-    #                     #     row.fees_category = trans_student.fee_category
-    #                     #     row.amount = trans_student.fee_amount
-    #                     #     row.gross_amount = trans_student.fee_amount
-    #                     # else:
-    #                     #     for comp in self.components:
-    #                     #         if comp.fees_category == trans_student.fee_category:
-    #                     #             comp.amount = trans_student.fee_amount
-    #                     #             comp.gross_amount = trans_student.fee_amount
-    #                     # trans_fee = frappe.new_doc("Fees")
-    #                     # trans_fee.student = self.student
-    #                     # trans_fee.posting_date = self.posting_date
-    #                     # trans_fee.due_date = self.due_date
-
-    #                     # trans_fee.student_name = self.student_name
-    #                     # trans_fee.program = self.program
-    #                     # trans_fee.student_batch = self.student_batch
-    #                     # # trans_fee.send_payment_request = self.send_payment_request
-    #                     # trans_fee.components = []
-    #                     # trans_fee.append('components', {
-    #                     #     "fees_category": trans_student.fee_category,
-    #                     #     "amount": trans_student.fee_amount,
-    #                     #     "gross_amount": trans_student.fee_amount
-    #                     # })
-    #                     # trans_fee.taxes_and_charges = self.taxes_and_charges
-                        
-    #                     # # trans_fee.validate()
-    #                     # trans_fee.save()
